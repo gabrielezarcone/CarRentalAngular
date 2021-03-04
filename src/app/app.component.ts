14d87983e28af2f85bc553fd6fd36fcce81e6978
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-04 16:35:01 +0100

    API users usate per popolare i dati del componente tabella

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MyButtonConfig} from './basic-components/my-button/my-button.component';
 import {MyHeaders, MyOrder, TableConfig} from './basic-components/table/table.component';
+import {UsersService} from './Service/api-services/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private userService: UsersService){}
   title = 'CarRentalAngular';
   btnConfig: MyButtonConfig = new MyButtonConfig('hello', undefined, 'tree-fill');
   // Table --------------------------------------------------------------------------------------------------
   order: MyOrder = new MyOrder('name', 'asc');
-  headers: MyHeaders[] = [new MyHeaders('name', 'Nome'), new MyHeaders('surname', 'Cognome'), new MyHeaders('age', 'Età')];
+  headers: MyHeaders[] = [new MyHeaders('name', 'Nome'), new MyHeaders('surname', 'Cognome')];
   tableConfig: TableConfig = new TableConfig(this.headers, this.order);
-  tableData: any[] = [{name: 'a', surname: 'A', age: 22}, {name: 'b', surname: 'B', age: 32}, {name: 'c', surname: 'C', age: 20}, {name: 'a', surname: 'A', age: 22}, {name: 'b', surname: 'B', age: 32}, {name: 'c', surname: 'C', age: 20}, {name: 'a', surname: 'A', age: 22}, {name: 'b', surname: 'B', age: 32}, {name: 'c', surname: 'C', age: 20}, {name: 'a', surname: 'A', age: 22}, {name: 'b', surname: 'B', age: 32}, {name: 'c', surname: 'C', age: 20} ];
+  tableData: any[];
   // --------------------------------------------------------------------------------------------------
+  ngOnInit(): void {
+    this.userService.getAll().subscribe(
+      data => this.tableData = data
+    );
+  }
 }
