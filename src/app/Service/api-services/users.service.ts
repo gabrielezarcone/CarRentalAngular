1e91c29aa41d6c7b9a61ec771772804740f6d544
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-04 17:01:25 +0100

    Strato di servizio per chiamare API users - CRUD

--- src/app/Service/api-services/users.service.ts
+++ src/app/Service/api-services/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {User} from '../../Model/User';
 
 @Injectable()
 export class UsersService {
@@ -8,7 +9,23 @@ export class UsersService {
 
   constructor(private http: HttpClient) { }
 
-  getAll(): Observable<any>{
+  getAll(): Observable<User[]>{
     return this.http.get<any>(this.baseUrl);
+  }
+
+  get(id: number): Observable<User>{
+    return this.http.get<any>(this.baseUrl + '/' + id);
+  }
+
+  create(user: User): Observable<User>{
+    return this.http.post<any>(this.baseUrl, user);
+  }
+
+  update(id: number, user: User): Observable<User>{
+    return this.http.put<any>(this.baseUrl + '/' + id, user);
+  }
+
+  delete(id: number): Observable<User>{
+    return this.http.delete<any>(this.baseUrl + '/' + id);
   }
 }
