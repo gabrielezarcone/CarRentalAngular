6a5ca1176ccf9193cfdaa04489ad0739d2d8a389
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-04 17:23:10 +0100

    Strato di servizio CRUD astratto per qualsiasi oggetto del model

--- src/app/Service/api-services/users.service.ts
+++ src/app/Service/api-services/users.service.ts
@@ -1,31 +1,12 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
 import {User} from '../../Model/User';
+import {AbstractApiService} from './abstractApiService';
 
 @Injectable()
-export class UsersService {
-  private baseUrl = 'http://localhost:8000/users/';
-
-  constructor(private http: HttpClient) { }
-
-  getAll(): Observable<User[]>{
-    return this.http.get<any>(this.baseUrl);
-  }
-
-  get(id: number): Observable<User>{
-    return this.http.get<any>(this.baseUrl + '/' + id);
-  }
-
-  create(user: User): Observable<User>{
-    return this.http.post<any>(this.baseUrl, user);
-  }
-
-  update(id: number, user: User): Observable<User>{
-    return this.http.put<any>(this.baseUrl + '/' + id, user);
-  }
-
-  delete(id: number): Observable<User>{
-    return this.http.delete<any>(this.baseUrl + '/' + id);
+export class UsersService extends AbstractApiService<User>{
+  baseUrl = 'http://localhost:8000/users/';
+  constructor(protected http: HttpClient) {
+    super(http);
   }
 }
