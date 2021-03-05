9bd5583c2606106f4d1e68a586d5ba683ec64eb9
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-05 15:38:05 +0100

    Componente NavBar

--- src/app/basic-components/basic-components.module.ts
+++ src/app/basic-components/basic-components.module.ts
@@ -5,11 +5,12 @@ import { TableComponent } from './table/table.component';
 import {FormsModule} from '@angular/forms';
 import { PaginationPipePipe } from './pagination-pipe.pipe';
 import { CrudButtonsComponent } from './crud-buttons/crud-buttons.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 
 @NgModule({
-  declarations: [MyButtonComponent, TableComponent, PaginationPipePipe, CrudButtonsComponent],
+  declarations: [MyButtonComponent, TableComponent, PaginationPipePipe, CrudButtonsComponent, NavBarComponent],
   imports: [
     CommonModule,
     FormsModule
