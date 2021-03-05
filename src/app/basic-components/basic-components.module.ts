15f0b26f39b96fcf0a9d5bc231b19a8891b5287a
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-05 10:44:04 +0100

    Component CrudButtons

--- src/app/basic-components/basic-components.module.ts
+++ src/app/basic-components/basic-components.module.ts
@@ -4,11 +4,12 @@ import { MyButtonComponent } from './my-button/my-button.component';
 import { TableComponent } from './table/table.component';
 import {FormsModule} from '@angular/forms';
 import { PaginationPipePipe } from './pagination-pipe.pipe';
+import { CrudButtonsComponent } from './crud-buttons/crud-buttons.component';
 
 
 
 @NgModule({
-  declarations: [MyButtonComponent, TableComponent, PaginationPipePipe],
+  declarations: [MyButtonComponent, TableComponent, PaginationPipePipe, CrudButtonsComponent],
   imports: [
     CommonModule,
     FormsModule
