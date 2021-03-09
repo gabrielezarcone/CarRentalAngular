39a709ffa35a24ca9c1793b8ef0f328f0514752a
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-09 17:08:03 +0100

    MyButtonConfig.routerLink come lambda function

--- src/app/basic-components/my-button/my-button.component.ts
+++ src/app/basic-components/my-button/my-button.component.ts
@@ -4,11 +4,11 @@ export class MyButtonConfig {
   customCssClass = 'btn-primary';
   text: string;
   icon: string; // nome dell'icona di bootstrap icon. Per esempio icon=alarm equivale a <i class="bi-alarm"></i>
-  routerLink?: string; // contiene la stringa di routerLink usata dal padre per applicare l'attributo routerLink
+  routerLink?: (...args: any) => string; // contiene la stringa di routerLink usata dal padre per applicare l'attributo routerLink
   click: (...args: any) => void; // contiene la funzione di click usata dal padre per applicare l'attributo (click)
   visible?: (row) => boolean;
 
-  constructor(text?: string, cssClass?: string, icon?: string, routerLink?: string, click?: (...args: any) => void, visible?: (row) => boolean) {
+  constructor(text?: string, cssClass?: string, icon?: string, routerLink?: (...args: any) => string, click?: (...args: any) => void, visible?: (row) => boolean) {
     if (text) {this.text = text; }
     if (cssClass) { this.customCssClass = cssClass; }
     if (icon) { this.icon = icon; }
