c795fecb5d2f3a0ed77bf4af2846e37d31c938be
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-09 12:36:12 +0100

    Mostra la pagina delle prenotazioni in base a utente

--- src/app/Components/car-rental-pages/lista-prenotazioni/lista-prenotazioni.component.ts
+++ src/app/Components/car-rental-pages/lista-prenotazioni/lista-prenotazioni.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Prenotazione} from '../../../Model/Prenotazione';
+import {PrenotazioneService} from '../../../Service/api-services/prenotazione.service';
+import {MyHeaders, MyOrder, TableConfig} from '../../../basic-components/table/table.component';
+import {ActivatedRoute} from '@angular/router';
+import {MyButtonConfig} from '../../../basic-components/my-button/my-button.component';
 
 @Component({
   selector: 'app-lista-prenotazioni',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-prenotazioni.component.scss']
 })
 export class ListaPrenotazioniComponent implements OnInit {
+  prenotazioniUtente: Prenotazione[];
+  id = +this.route.snapshot.paramMap.get('id');
+  // Tabella ******************************************
+  private headers: MyHeaders[] = [
+    new MyHeaders('inizio', 'Inizio'),
+    new MyHeaders('fine', 'Fine'),
+    new MyHeaders('stato', 'Stato'),
+    new MyHeaders('auto', 'Auto')
+  ];
+  private order = new MyOrder('inizio', 'asc');
+  crudBtns: MyButtonConfig[] = [];
+  tableConfig = new TableConfig(this.headers, this.order);
+  // ****************************************** Tabella
 
-  constructor() { }
+  constructor(
+    private prenotazioneService: PrenotazioneService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.prenotazioneService.getByUser(this.id).subscribe(
+      data => this.prenotazioniUtente = data
+    );
   }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeAdminComponent} from './Components/car-rental-pages/home-admin/home-admin.component';
 import {ModificaUserComponent} from './Components/car-rental-pages/modifica-user/modifica-user.component';
 import {EliminaUserComponent} from './Components/car-rental-pages/elimina-user/elimina-user.component';
+import {ListaPrenotazioniComponent} from './Components/car-rental-pages/lista-prenotazioni/lista-prenotazioni.component';
 
 const routes: Routes = [
   {path: 'homeAdmin', component: HomeAdminComponent},
   {path: 'modifica/user/:id', component: ModificaUserComponent},
-  {path: 'elimina/user/:id', component: EliminaUserComponent}
+  {path: 'elimina/user/:id', component: EliminaUserComponent},
+  {path: 'prenotazioni/user/:id', component: ListaPrenotazioniComponent}
 ];
 
 @NgModule({
