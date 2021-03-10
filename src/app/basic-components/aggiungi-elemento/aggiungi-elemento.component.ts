c7350c18425e9c4c9b851a92571b8145b0e4a9ac
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-10 17:37:00 +0100

    AggiungiElementoComponent

---
+++ src/app/basic-components/aggiungi-elemento/aggiungi-elemento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aggiungi-elemento',
+  templateUrl: './aggiungi-elemento.component.html',
+  styleUrls: ['./aggiungi-elemento.component.scss']
+})
+export class AggiungiElementoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
