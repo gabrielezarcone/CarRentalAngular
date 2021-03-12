7472991616ddf76de3d271d05befb8a0c5955bce
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-12 15:12:24 +0100

    TokenInterceptorService

--- src/app/Service/basic-services/basic-services.module.ts
+++ src/app/Service/basic-services/basic-services.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AuthService} from './AuthService/auth.service';
+import {TokenInterceptorService} from './HttpInterceptors/token-interceptor.service';
 
 
 
 @NgModule({
   declarations: [],
   providers: [
-    AuthService
+    AuthService,
+    TokenInterceptorService
   ],
   imports: [
     CommonModule
