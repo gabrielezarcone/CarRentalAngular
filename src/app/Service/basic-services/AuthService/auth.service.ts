ca5d1d3745d3939fe96b83fe9a5d537628058c88
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-12 15:06:56 +0100

    Metodi di AuthService

--- src/app/Service/basic-services/AuthService/auth.service.ts
+++ src/app/Service/basic-services/AuthService/auth.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../../../Model/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  baseUrl  = 'http://localhost:8000/register';
+
+  constructor(private http: HttpClient) { }
+
+  private static setSession(authRes): void{
+    localStorage.setItem('jwtToken', authRes);
+  }
+
+  login(email: string, password: string): void{
+    this.http.post<User>(this.baseUrl, {email, password}).subscribe(
+      res => AuthService.setSession(res)
+    );
+  }
+
+  logout(): void{
+    localStorage.removeItem('jwtToken');
+  }
+
+  getToken(): string{
+    return localStorage.getItem('jwtToken');
+  }
 }
