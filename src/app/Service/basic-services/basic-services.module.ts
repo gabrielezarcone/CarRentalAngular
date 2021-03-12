31107f4ce8cbea387a49c1f81573a568c5bae00b
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-12 16:44:44 +0100

    AuthGuardsService

--- src/app/Service/basic-services/basic-services.module.ts
+++ src/app/Service/basic-services/basic-services.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {AuthService} from './AuthService/auth.service';
 import {TokenInterceptorService} from './HttpInterceptors/token-interceptor.service';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {AuthGuardsService} from './AuthGuards/auth-guards.service';
 
 
 
@@ -10,6 +11,7 @@ import {HTTP_INTERCEPTORS} from '@angular/common/http';
   declarations: [],
   providers: [
     AuthService,
+    AuthGuardsService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptorService,
