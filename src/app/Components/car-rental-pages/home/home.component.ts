d7d3c5aa1ef066ba868d9ba5ac116756c0f93ca3
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-15 11:34:31 +0100

    HomeComponent reindirizza la pagina in base al ruolo dell'utente loggato

--- src/app/Components/car-rental-pages/home/home.component.ts
+++ src/app/Components/car-rental-pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../../Service/basic-services/AuthService/auth.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.auth.loggedUserRole().subscribe(
+      ruolo => {
+        if (ruolo.ruolo === 'ROLE_ADMIN'){
+          this.router.navigateByUrl('/homeAdmin');
+        }
+        else if (ruolo.ruolo === 'ROLE_CUSTOMER'){
+          this.router.navigateByUrl('/homeCustomer');
+        }
+        else {
+          this.router.navigateByUrl('/login');
+        }
+      }
+    );
   }
 
 }
